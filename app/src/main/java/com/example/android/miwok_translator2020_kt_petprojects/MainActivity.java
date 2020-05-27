e90c8963a91f35d34020e8cfaa0093bbafe7caa4
tharaka <tharakakushan@gmail.com>
2020-05-27 11:06:04 +0530

    Commit initial : connecting IDE to Version Control System

---
+++ app/src/main/java/com/example/android/miwok_translator2020_kt_petprojects/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.miwok_translator2020_kt_petprojects;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
