d2130093ce519f525e82b38f28d01bb7918f359f
tharaka <tharakakushan@gmail.com>
2020-05-27 11:21:39 +0530

    DesignedGroups- Colors,Family,Numbers,Phrases activity groups

---
+++ app/src/main/java/com/example/android/miwok_translator2020_kt_petprojects/ColorsActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.miwok_translator2020_kt_petprojects;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ColorsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_colors2);
+    }
+}
