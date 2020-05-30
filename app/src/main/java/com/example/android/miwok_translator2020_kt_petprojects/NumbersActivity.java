0b010d0c0e5e1b6351d652f07c976cedde3278fa
tharaka <tharakakushan@gmail.com>
2020-05-30 09:03:48 +0530

    Logcat Messages generated about the English Numbers Array:out of bound clear

--- app/src/main/java/com/example/android/miwok_translator2020_kt_petprojects/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok_translator2020_kt_petprojects/NumbersActivity.java
@@ -3,6 +3,7 @@ package com.example.android.miwok_translator2020_kt_petprojects;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -25,6 +26,19 @@ public class NumbersActivity extends AppCompatActivity {
         englishNumberWords[7] = "Eight";
         englishNumberWords[8] = "Nine";
         englishNumberWords[9] = "Ten";
+
+        //debug || Log views verbose: just messages to pass in Logcat
+
+        Log.v("NumbersActivity","Number at index 0: " + englishNumberWords[0]);
+        Log.v("NumbersActivity","Number at index 1" + englishNumberWords[1]);
+        Log.v("NumbersActivity","Number at index 2: " + englishNumberWords[2]);
+        Log.v("NumbersActivity","Number at index 3" + englishNumberWords[3]);
+        Log.v("NumbersActivity","Number at index 4: " + englishNumberWords[4]);
+        Log.v("NumbersActivity","Number at index 5" + englishNumberWords[5]);
+        Log.v("NumbersActivity","Number at index 6: " + englishNumberWords[6]);
+        Log.v("NumbersActivity","Number at index 7" + englishNumberWords[7]);
+        Log.v("NumbersActivity","Number at index 8: " + englishNumberWords[8]);
+        Log.v("NumbersActivity","Number at index 9" + englishNumberWords[9]);
 
     }
 }
