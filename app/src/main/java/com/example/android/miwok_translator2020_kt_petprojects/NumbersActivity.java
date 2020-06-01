de5b3de9f680658083417e5dd84f03109708086f
tharaka <tharakakushan@gmail.com>
2020-06-01 09:21:16 +0530

    Write word for for Numbers

--- app/src/main/java/com/example/android/miwok_translator2020_kt_petprojects/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok_translator2020_kt_petprojects/NumbersActivity.java
@@ -20,20 +20,20 @@ public class NumbersActivity extends AppCompatActivity {
 
         //creating arrayList
         ArrayList<Word> words = new ArrayList<Word>();
-        //words.add(0, "One");
-        Word w = new Word("lutti", "one");
-        words.add(w);
+
+        //words.add(0, "One")
+        words.add(new Word("lutti", "one"));
+        words.add(new Word("two", "otiiko"));
+        words.add(new Word("three", "tolookosu"));
+        words.add(new Word("four", "oyyisa"));
+        words.add(new Word("five", "massokka"));
+        words.add(new Word("six", "temmokka"));
+        words.add(new Word("seven", "kenekaku"));
+        words.add(new Word("eight", "kawinta"));
+        words.add(new Word("nine", "wo’e"));
+        words.add(new Word("ten", "na’aacha"));
 
 
-    /*    words.add(1, "Two");
-        words.add(2, "Three");
-        words.add(3, "Four");
-        words.add(4, "Five");
-        words.add(5, "Six");
-        words.add(6, "Seven");
-        words.add(7, "Eight");
-        words.add(8, "Nine");
-        words.add(9, "Ten");*/
 
         //Add a log in verbose to determine the process that ongoing
         //make a for loop to make looping and show Log message to this
@@ -42,7 +42,7 @@ public class NumbersActivity extends AppCompatActivity {
         }
 
         //created array ada[ter for recyclr view | memory optimization
-        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<>(this, R.layout.list_item , words);
+        ArrayAdapter<Word> itemsAdapter = new ArrayAdapter<Word>(this, R.layout.list_item , words);
 
         ListView listView = findViewById(R.id.listView);
 
