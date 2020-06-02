34a79dffdbfe9d435507f6fc6c8e0efbe2d03ce9
tharaka <tharakakushan@gmail.com>
2020-06-02 18:56:52 +0530

    COLORS LIST PREPARING :  Created a ArrayList Class new Object called "NumberList"  Add the all colours Miwok translation and Default translation to the array

--- app/src/main/java/com/example/android/miwok_translator2020_kt_petprojects/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok_translator2020_kt_petprojects/ColorsActivity.java
@@ -4,11 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class ColorsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colors);
+
+        /*first have to create a arraylist for the colours,
+        I am using previous word object of ArrayList to store the
+        colours of Miwok Language*/
+        ArrayList<Word> numberList = new ArrayList<Word>();
+
+        /*Now second step is store the list of Names in colours(these words will auto
+        store in Word oject when array is running)*/
+        numberList.add(new Word("red", "weṭeṭṭi"));
+        numberList.add(new Word("green", "weṭeṭṭi"));
+        numberList.add(new Word("brown", "ṭakaakki"));
+        numberList.add(new Word("gray", "ṭopoppi"));
+        numberList.add(new Word("black", "kululli"));
+        numberList.add(new Word("white", "kelelli"));
+        numberList.add(new Word("dusty yellow", "ṭopiisә"));
+        numberList.add(new Word("mustard yellow", "chiwiiṭә"));
     }
 }
