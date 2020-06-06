95ac799cd43bd47d276ecfc9a1fc50a7d9e8be04
tharaka <tharakakushan@gmail.com>
2020-06-06 07:23:27 +0530

    Family Members Images added

--- app/src/main/java/com/example/android/miwok_translator2020_kt_petprojects/FamiliActivities.java
+++ app/src/main/java/com/example/android/miwok_translator2020_kt_petprojects/FamiliActivities.java
@@ -22,16 +22,16 @@ public class FamiliActivities extends AppCompatActivity {
         ArrayList<Word> familyList = new ArrayList<Word>();
 
         /*After that have to add family words to the familyList ArrayList that created aove*/
-        familyList.add(new Word("father", "әpә"));
-        familyList.add(new Word("mother ", "әṭa"));
-        familyList.add(new Word("son  ", "angsi"));
-        familyList.add(new Word("daughter", "tune"));
-        familyList.add(new Word("older brother", "taachi"));
-        familyList.add(new Word("younger brother", "chalitti"));
-        familyList.add(new Word("older sister", "kolliti"));
-        familyList.add(new Word("younger sister", "kolliti"));
-        familyList.add(new Word("grandmother", "ama"));
-        familyList.add(new Word("grandfather", "paapa"));
+        familyList.add(new Word("father", "әpә",R.drawable.family_father));
+        familyList.add(new Word("mother ", "әṭa",R.drawable.family_mother));
+        familyList.add(new Word("son  ", "angsi",R.drawable.family_son));
+        familyList.add(new Word("daughter", "tune",R.drawable.family_daughter));
+        familyList.add(new Word("older brother", "taachi",R.drawable.family_older_brother));
+        familyList.add(new Word("younger brother", "chalitti",R.drawable.family_younger_brother));
+        familyList.add(new Word("older sister", "kolliti",R.drawable.family_older_sister));
+        familyList.add(new Word("younger sister", "kolliti",R.drawable.family_younger_sister));
+        familyList.add(new Word("grandmother", "ama",R.drawable.family_grandmother));
+        familyList.add(new Word("grandfather", "paapa",R.drawable.family_grandfather));
 
         /*Creating a ArrayAdapter to display the  words in a beautiful list*/
         WordAdapter familyAdapter = new WordAdapter(this, familyList);
